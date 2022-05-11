a81a80ff34ddeac7de1f1c34c5d6fc92b9239575
1005307004 <1005307004@L829WD210016.edu.cbe.ab.ca>
2022-05-11 13:38:03 -0600

    born into to the tundra, the king rules his land.

---
+++ GameOfLife/src/GameOfLife.java
@@ -0,0 +1,41 @@
+
+public class GameOfLife {
+
+	private int column;
+	private int row;
+	
+	public GameOfLife(int column, int row) {
+		this.column = column;
+		this.row = row;
+	}
+
+	public int getColumns() {
+		return column;
+	}
+
+	public int getRows() {
+		return row;
+	}
+
+	public boolean isAlive(int column, int row) {
+		
+		return false;
+	}
+
+
+	public int getNeighbourCount(int column, int row) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void calculateNextGeneration() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAlive(int column, int row, boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
